3e55733a39e4b441c093d31bcaae612aba6d5d03
Kiruthika <kiruthi2541@gmail.com>
2026-04-25 19:38:43 +0530

    Expense details can be updated and deleted

--- expense_tracker_app/urls.py
+++ expense_tracker_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
   path('',views.home,name='home'),
   path('add-expense',views.new_expense,name='new_expense'),
   path('add-category',views.new_category,name='new_category'),
+  path('update-expense/<int:exp_id>',views.update_expense,name='update_expense'),
+  path('delete-expense/<int:exp_id>',views.delete_expense,name='delete_expense'),
+  
   ]
--- expense_tracker_app/views.py
+++ expense_tracker_app/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from expense_tracker_app.forms import ExpenseForm, CategoryForm
 from expense_tracker_app.models import AddExpense, AddCategory
 
-# Create your views here.
+  # HOME PAGE
 def home(request):
   expense = AddExpense.objects.all()
   category = AddCategory.objects.all()
   return render(request,'home.html',{'expense':expense,'category':category})
   
-  
+  # ADD EXPENSE
 def new_expense(request):
   if request.method == 'POST':
     form = ExpenseForm(request.POST)
@@ -20,7 +20,7 @@ def new_expense(request):
     form = ExpenseForm()
   return render(request,'expense.html',{'form':form})
   
-  
+  # ADD CATEGORY
 def new_category(request):
   if request.method == 'POST':
     form = CategoryForm(request.POST)
@@ -30,4 +30,28 @@ def new_category(request):
   else:
     form = CategoryForm()
   return render(request,'category.html',{'form':form})
+  
+  # UPDATE EXPENSE
+def update_expense(request,exp_id):
+  required_expense = get_object_or_404(AddExpense,id = exp_id)
+  if request.method == 'POST':
+    form = ExpenseForm(request.POST,instance = required_expense)
+    if form.is_valid():  
+      form.save()
+      return redirect('home')
+  else:
+    form = ExpenseForm(instance = required_expense)
+  return render(request,'updateexpense.html',{'form':form})
+  
+  # DELETE EXPENSE
+def delete_expense(request,exp_id):
+  required_expense = get_object_or_404(AddExpense,id = exp_id)
+  if request.method == 'POST':
+    required_expense.delete()
+    return redirect('home')
+  
+  return render(request,'deleteexpense.html',{'form':required_expense})
+  # DELETE CATEGORY
+  # LOGIN
+  # REGISTER NEW USER 
   
