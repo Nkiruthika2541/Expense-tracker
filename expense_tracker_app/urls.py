7c84c798e430ee7e5faf6461c406083c4a7b6a20
Kiruthika <kiruthi2541@gmail.com>
2026-04-26 20:04:50 +0530

    View all expenses, Update or delete the category and a minimal dashboard view

--- expense_tracker_app/urls.py
+++ expense_tracker_app/urls.py
@@ -3,10 +3,13 @@ from expense_tracker_app import views
 
 
 urlpatterns = [
-  path('',views.home,name='home'),
+  path('',views.dashboard,name='dashboard'),
   path('add-expense',views.new_expense,name='new_expense'),
   path('add-category',views.new_category,name='new_category'),
+  path('view-expense',views.view_expense,name='view_expense'),
   path('update-expense/<int:exp_id>',views.update_expense,name='update_expense'),
   path('delete-expense/<int:exp_id>',views.delete_expense,name='delete_expense'),
+  path('update-category/<int:catg_id>',views.update_category,name='update_category'),
+  path('delete-category/<int:catg_id>',views.delete_category,name='delete_category'),
   
   ]
--- expense_tracker_app/views.py
+++ expense_tracker_app/views.py
@@ -1,13 +1,23 @@
+from django.db.models import Sum, Avg, Min, Max
+from django.utils import timezone
 from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from expense_tracker_app.forms import ExpenseForm, CategoryForm
 from expense_tracker_app.models import AddExpense, AddCategory
 
-  # HOME PAGE
-def home(request):
+  # HOME PAGE / DASHBOARD
+def dashboard(request):
+  total_spent = AddExpense.objects.aggregate(t=Sum('amount'))
+  all_categories = AddCategory.objects.all()
+  today = timezone.now().date()
+  recent_expenses = AddExpense.objects.filter(date__lte=today).order_by('-date')[:10]
+
+  return render(request,'dashboard.html',{'total_spent':total_spent,'all_categories':all_categories,'expenses':recent_expenses})
+
+  # VIEW ALL EXPENSES
+def view_expense(request):
   expense = AddExpense.objects.all()
-  category = AddCategory.objects.all()
-  return render(request,'home.html',{'expense':expense,'category':category})
+  return render(request,'view_expense.html',{'expense':expense})
   
   # ADD EXPENSE
 def new_expense(request):
@@ -15,7 +25,7 @@ def new_expense(request):
     form = ExpenseForm(request.POST)
     if form.is_valid():  
       form.save()
-      return redirect('home')
+      return redirect('dashboard')
   else:
     form = ExpenseForm()
   return render(request,'expense.html',{'form':form})
@@ -26,7 +36,7 @@ def new_category(request):
     form = CategoryForm(request.POST)
     if form.is_valid():  
       form.save()
-      return redirect('home')
+      return redirect('dashboard')
   else:
     form = CategoryForm()
   return render(request,'category.html',{'form':form})
@@ -38,7 +48,7 @@ def update_expense(request,exp_id):
     form = ExpenseForm(request.POST,instance = required_expense)
     if form.is_valid():  
       form.save()
-      return redirect('home')
+      return redirect('dashboard')
   else:
     form = ExpenseForm(instance = required_expense)
   return render(request,'updateexpense.html',{'form':form})
@@ -48,10 +58,32 @@ def delete_expense(request,exp_id):
   required_expense = get_object_or_404(AddExpense,id = exp_id)
   if request.method == 'POST':
     required_expense.delete()
-    return redirect('home')
+    return redirect('dashboard')
   
   return render(request,'deleteexpense.html',{'form':required_expense})
+  
+  # UPDATE EXSISTING CATEGORY
+def update_category(request,catg_id):
+  required_category = get_object_or_404(AddCategory,id = catg_id)
+  if request.method == 'POST':
+    form = CategoryForm(request.POST,instance = required_category)
+    if form.is_valid():  
+      form.save()
+      return redirect('dashboard')
+  else:
+    form = CategoryForm(instance = required_category)
+  return render(request,'updatecategory.html',{'form':form})
+  
   # DELETE CATEGORY
+def delete_category(request,catg_id):
+  required_category = get_object_or_404(AddCategory,id = catg_id)
+  if request.method == 'POST':
+    required_category.delete()
+    return redirect('dashboard')
+  
+  return render(request,'deletecategory.html',{'form':required_category})
+
+
   # LOGIN
   # REGISTER NEW USER 
   
